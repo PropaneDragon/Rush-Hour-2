f277c17300f4e88f73814f95033b7350f7639ba6
Sean <thingshappen.xuras@gmail.com>
2018-07-10 23:05:04 +0100

    Starting the work on determining whether a building is a good choice or not.

--- RushHour2.Citizens/Location/GlobalLocationHandler.cs
+++ RushHour2.Citizens/Location/GlobalLocationHandler.cs
@@ -1,9 +1,40 @@
 ﻿using RushHour2.Core.Settings;
+using System.Collections.Generic;
 
 namespace RushHour2.Citizens.Location
 {
     public static class GlobalLocationHandler
     {
+        private static Dictionary<Citizen.AgeGroup, string[]> _badBuildingNames = new Dictionary<Citizen.AgeGroup, string[]>()
+        {
+            {
+                Citizen.AgeGroup.Child,
+                new[]
+                {
+                    "bar", "club", "night"
+                }
+            },
+            {
+                Citizen.AgeGroup.Senior,
+                new []
+                {
+                    "playground", "children"
+                }
+            },
+            {
+                Citizen.AgeGroup.Teen,
+                new []
+                {
+                    "playground", "children"
+                }
+            }
+        };
+
+        private static string[] _globalBadBuildingNames = new[]
+        {
+            "parking", "carpark", "car_park", "parking_lot", "parking lot", "car park"
+        };
+
         public static bool ShouldMove()
         {
             var vehicleCount = VehicleManager.instance.m_vehicleCount;
@@ -21,12 +52,43 @@ namespace RushHour2.Citizens.Location
             return randomPercentage > (percentageCitizens > percentageVehicles ? percentageCitizens : percentageVehicles);
         }
 
+        public static bool GoodBuildingToVisit(ushort buildingId, ref Citizen citizen)
+        {
+            if (buildingId != 0)
+            {
+                var building = BuildingManager.instance.m_buildings.m_buffer[buildingId];
+
+                return GoodBuildingToVisit(ref building, ref citizen);
+            }
+
+            return false;
+        }
+
         public static bool GoodBuildingToVisit(ref Building building, ref Citizen citizen)
         {
             var info = building.Info;
             var @class = info.m_class;
             var ageGroup = Citizen.GetAgeGroup(citizen.Age);
+            var buildingName = info.name.ToLower();
 
+            if (_badBuildingNames.ContainsKey(ageGroup))
+            {
+                foreach (var badName in _badBuildingNames[ageGroup])
+                {
+                    if (buildingName.Contains(badName))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            foreach (var globalBadName in _globalBadBuildingNames)
+            {
+                if (buildingName.Contains(globalBadName))
+                {
+                    return false;
+                }
+            }
 
             return true;
         }
