1543e36f1ede8a5e77204b4500ee0c2c1a87e8ec
Sean <thingshappen.xuras@gmail.com>
2018-07-15 00:07:20 +0100

    Beginnings of localisation with a very basic auto translation class to pull down automatic changes for missing translations (from existing translation files)

---
+++ RushHour2.Localisation/Language/GoogleTranslate.cs
@@ -0,0 +1,71 @@
+﻿using ColossalFramework.HTTP;
+using RushHour2.Core.Reporting;
+using System.Text.RegularExpressions;
+using System.Threading;
+using UnityEngine;
+
+namespace RushHour2.Localisation.Language
+{
+    public static class GoogleTranslate
+    {
+        public static string GetTranslationFor(string text, string toIsoCode, string fromIsoCode = "en-gb")
+        {
+            LoggingWrapper.Log(LoggingWrapper.LogArea.File, LoggingWrapper.LogType.Message, $"Translating {text} into {toIsoCode} from {fromIsoCode}...");
+
+            var safeText = WWW.EscapeURL(text);
+            var request = new Request("get", $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromIsoCode}&tl={toIsoCode}&dt=t&q={safeText}");
+            request.AddHeader("Accept-Language", "en");
+            request.acceptGzip = false;
+
+            request.Send();
+            
+            while (!request.isDone)
+            {
+                Thread.Sleep(10);
+            }
+
+            if (request.response != null)
+            {
+                return GetTranslation(request.response);
+            }
+
+            return null;
+        }
+
+        private static string GetTranslation(Response response)
+        {
+            if (response != null)
+            {
+                var jsonString = response.Text;
+                if (jsonString != null)
+                {
+                    var jsonNode = SimpleJSON.JSON.Parse(jsonString);
+                    if (jsonNode != null && jsonNode.Count > 0)
+                    {
+                        var translationGroups = jsonNode[0];
+                        if (translationGroups != null && translationGroups.Count > 0)
+                        {
+                            var fullString = "";
+
+                            foreach (var translation in translationGroups.Children)
+                            {
+                                if (translation != null && translation.Count > 1)
+                                {
+                                    var translationText = translation[0];
+                                    if (translationText != null)
+                                    {
+                                        fullString += Regex.Unescape(translationText.Value);
+                                    }
+                                }
+                            }
+
+                            return fullString + "[*]";
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
