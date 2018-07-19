4778148b631bbd69280303a8fdd5431359f353d5
Sean <thingshappen.xuras@gmail.com>
2018-07-19 17:31:02 +0100

    Issue #15: Hopeful fix for save games that include the old Rush Hour citizen messages that appear on the chirper display.

---
+++ RushHour/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RushHour")]
+[assembly: AssemblyDescription("Legacy support for Rush Hour II")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RushHour")]
+[assembly: AssemblyCopyright("Copyright © PropaneDragon 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6182996d-54c7-45c4-aba8-55082ccf3d8f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.6514.37964")]
+[assembly: AssemblyFileVersion("0.9.6514.37964")]
