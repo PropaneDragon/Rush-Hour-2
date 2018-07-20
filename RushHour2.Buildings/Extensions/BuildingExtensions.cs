a4a941fe74de5d462b25ccf504ac182d6fac6787
Sean <thingshappen.xuras@gmail.com>
2018-07-20 12:15:15 +0100

    Stopping exceptions when building enter doors don't exist.

--- RushHour2.Buildings/Extensions/BuildingExtensions.cs
+++ RushHour2.Buildings/Extensions/BuildingExtensions.cs
@@ -1,10 +1,12 @@
-﻿namespace RushHour2.Buildings.Extensions
+﻿using System.Linq;
+
+namespace RushHour2.Buildings.Extensions
 {
     public static class BuildingExtensions
     {
-        public static bool Enterable(this Building building) => building.Info.m_enterDoors.Length > 0;
+        public static bool Enterable(this Building building) => building.Info?.m_enterDoors != null && building.Info.m_enterDoors.Length > 0;
 
-        public static bool Visitable(this Building building) => building.Enterable() || building.Info.m_specialPlaces.Length > 0;
+        public static bool Visitable(this Building building) => building.Enterable() || (building.Info?.m_specialPlaces != null && building.Info.m_specialPlaces.Length > 0);
 
         public static ItemClass.Service Service(this Building building) => building.Info.m_class.m_service;
 
